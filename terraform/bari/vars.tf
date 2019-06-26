baa505276dfaf87cb5e613c86820c5cd390a8f39
Gianmauro Cuccuru <gmauro@gmail.com>
2019-06-26 11:12:16 +0200

    add bari configuration

--- terraform/bari/vars.tf
+++ terraform/bari/vars.tf
@@ -1,32 +1,32 @@
 variable "nfs_disk_size" {
-  default = 3
+  default = 200
 }
 
 variable "flavors" {
   type = "map"
   default = {
-    "central-manager" = "m1.medium"
-    "exec-node" = "m1.medium"
-    "nfs-server" = "m1.medium"
+    "central-manager" = "large"
+    "exec-node" = "xxlarge"
+    "nfs-server" = "large"
   }
 }
 
 variable "exec_node_count" {
-  default = 2
+  default = 5
 }
 
 variable "image" {
   type = "map"
   default = {
-    "name" = "vggp-v31-j124"
-    "image_source_url" = "https://usegalaxy.eu/static/vgcn/vggp-v31-j124-5f1806ab2120-master.raw"
+    "name" = "vggp-v31-j121"
+    "image_source_url" = "https://usegalaxy.eu/static/vgcn/vggp-v31-j121-77fa5971e513-master.raw"
     "container_format" = "bare"
     "disk_format" = "raw"
    }
 }
 
 variable "public_key" {
-  default = "ssh-rsa blablablabla..."
+  default =  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC1NsmuU6ETRGMk1veC5jyx/bRZ3WTjgoxVUcWAFizpvPGWdJc2x0ZW0dMr9WDSlqXkQHf4FvRiqxtL9Q8Kej8odESM7ej6y1/huGfQTGtHnCOV2P4MC+nNaNim0KzEUwgQ9MNdMUnfhJcmRNHIIUVFtxEfC2bhthoBpEN6WMehRgn7E91e9W5GHA7nmHmQCDTFGkPGyM3FTPsEarH9P4hjXlfp9pRb/vlhm5xbHEgKZU1iIDrNDZNeMfZlK7Ja1u9rCzAavwzNkwcLt3/dMoXuiDXTFtlzTM83bseHseCLpczJuwhW62DtitX4K/dO6zCKmcv1EUt2SEY2PsnZ0sZypDCtYq+lsCTUukp8E/DPzSsd61vfHEZBMzRw7bxU7ELH/TlcTRs4UmAYXA7phkmetIVq2Y9vNni1UP04dwP9rvlqdYXNDemIMs4AC48SDnrhZ47zSP0S6EMvKhcZcSWhAKKMr0p+3OHrifNXhLcLoz/+lsM1z/ev1YYeigkyXjVYeWGLy8fYSG5yEp27mSWdde1frGnp4Bl0t9FemcBI+AKvugwZiHNlUVI3m3XyP6tygK+MdMPQp6NwNWHKYUPFRZvRSnOEOoj6B9RRUEzW9XWR8Np1iuAf76UtuW4r/nwKlKKwU/uF7oKaRuiIUAYzGdyQrcZuBp3l4vV7h7bptw== gmauro@informatik.uni-freiburg.de"
 }
 
 variable "name_prefix" {
@@ -34,7 +34,7 @@ variable "name_prefix" {
 }
 
 variable "name_suffix" {
-  default = ".usegalaxy.eu"
+  default = ".cloud.ba.infn.it"
 }
 
 variable "secgroups_cm" {
@@ -55,15 +55,15 @@ variable "secgroups" {
 }
 
 variable "public_network" {
-  default  = "public"
+  default  = "public_net"
 }
 
 variable "private_network" {
   type = "map"
   default  = {
-    name = "vgcn-private"
-    subnet_name = "vgcn-private-subnet"
-    cidr4 = "192.168.199.0/24"
+    name = "private_net"
+    subnet_name = "private_subnet"
+    cidr4 = "172.30.63.0/24"
   }
 }
 
